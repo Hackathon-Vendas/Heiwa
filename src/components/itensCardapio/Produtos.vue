fa5bb7c053dd31785c744f16d7c88b2e230fcfe3
vallentini <vallentinigabrielli.silva@gmail.com>
2024-10-17 20:08:51 -0300

    FEAT:Desenvolvendo as páginas produtos #11

---
+++ src/components/itensCardapio/Produtos.vue
@@ -0,0 +1,139 @@
+<script setup>
+defineProps(['name', 'description', 'imagem', 'unit', 'price','title']);
+</script>
+<template>
+    <main>
+      <div class="topo">
+        <img :src=imagem class="banner">
+        <h2>{{ title }}</h2>
+      </div>
+        <div class="produtos">
+            <div class="item">
+        <img :src=imagem class="image">
+        <div class="details">
+          <div class="name">
+            <h3>{{ name }}</h3>
+            <p class="unit">{{ unit }}</p>
+            <p class="price">{{ price }}</p>
+          </div>
+          <p class="description">{{ description }}</p>
+        </div>
+            </div>
+      </div>
+    </main>
+</template>
+
+
+<style scoped>
+main {
+  width: 60%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 8%;
+  margin-left: 27%;
+}
+
+.topo{
+  margin-bottom: 10%;
+}
+.banner {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.produtos {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  border-radius: 14px;
+  width: 100%;
+  height: 200px;
+  background-color: rgba(69, 69, 69, 1);
+  color: rgba(255, 255, 255, 1);
+  margin-top: 3%;
+}
+
+.image {
+  width: 250px;
+  height: 230px;
+  border-radius: 18px;
+  margin-top: 19px;
+}
+
+details h3 {
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 1000;
+  font-size: 20px;
+  line-height: 27px;
+  letter-spacing: 0.25em;
+  color: #FFFFFF;
+  margin-bottom: 2%;
+}
+.price {
+  font-family: 'Inter', sans-serif, sans-serif;
+  font-style: normal;
+  font-weight: 1000;
+  font-size: 14px;
+  line-height: 27px;
+  letter-spacing: 0.25em;
+  color: #FFFFFF;
+  position: absolute;
+  right: 15%;
+
+}
+
+.unit {
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.25em;
+  color: #FFFFFF;
+  margin-left: 10px;
+}
+
+.description {
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+  letter-spacing: 0.25em;
+  color: #FFFFFF;
+  margin-top: 5%;
+}
+
+.details {
+  height: 80%;
+}
+
+.name {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+h2 {
+  position: absolute;
+  top: 270px;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 25px;
+  line-height: 30px;
+  text-align: center;
+  letter-spacing: 0.25em;
+  color: #FFFFFF;
+}
+</style>
---
+++ src/views/ProdutosView.vue
@@ -0,0 +1,118 @@
+<script setup>
+import Produtos from '@/components/itensCardapio/Produtos.vue';
+import { ref } from 'vue'
+import bannerEntradas from '../assets/bannerEntradas.png'
+import bannerPrincipais from '../assets/bannerPrincipais.png'
+import bannerAlcoolicas from '../assets/bannerAlcoolicas.png'
+import bannerBebidas from '../assets/bannerBebidas.png'
+import bannerSobremesas from '../assets/bannerSobremesas.png'
+
+import Sunomono from '../assets/Sunomono.png';
+import Gyoza from '../assets/gyoza.png';
+import Tataki from '../assets/tatakiAtum.png';
+import sushi1 from '../assets/sushi1.svg';
+import sushi2 from '../assets/sushi2.svg';
+import sushi3 from '../assets/sushi3.svg';
+import sushi4 from '../assets/sushi4.svg';
+import sushi5 from '../assets/sushi5.svg';
+import aguasg from '../assets/aguasg.png';
+import aguacg from '../assets/aguacg.png';
+import refri from '../assets/refri.png';
+import sucos from '../assets/sucos.png';
+import destilados from '../assets/destilados.png';
+import vinhos from '../assets/vinhos.png';
+import drinks from '../assets/drinks.png';
+import cervejas from '../assets/cervejas.png';
+import mochi from '../assets/Mochi.png';
+import sorvete from '../assets/Sorvete.png';
+import petitgateau from '../assets/PetitGateau.png';
+import tempura from '../assets/TempuradeSorvete.png';
+import dorayaki from '../assets/Dorayaki.png';
+import anmitsu from '../assets/Anmitsu.png';
+
+const Bentradas = ref([
+ {image: bannerEntradas , title: 'Entradas'}
+])
+
+const BPrincipais = ref([
+ {image: bannerPrincipais , title: 'Principais'}
+])
+
+const Balcoolicas = ref([
+ {image: bannerAlcoolicas , title: 'Bebidas'}
+])
+
+const Bbebidas = ref([
+ {image: bannerBebidas , title: 'Bebidas'}
+])
+
+const Bsobremesas = ref([
+ {image: bannerSobremesas , title: 'Pratos Principais'}
+])
+
+const bebidaAlcoolica = ref([
+  { name: 'Destilados', unit: '(A partir de:)', description: ' Bebidas alcoólicas obtidas por destilação, geralmente mais fortes, feitas a partir de grãos, frutas ou cana-de-açúcar.', price: 'R$40,00', image: destilados },
+  { name: 'Vinhos', unit: '(A partir de:)', description: 'Bebidas fermentadas feitas a partir de uvas.', price: 'R$70,00', image: vinhos },
+  { name: 'Drinks', unit: '(A partir de:)', description: 'Misturas de bebidas alcoólicas com outros ingredientes.', price: 'R$25,50', image: drinks },
+  { name: 'Cervejas', description: 'Bebidas fermentadas à base de cevada e lúpulo, com estilos variados.', unit: '(A partir de:)', price: 'R$12,50', image: cervejas },
+])
+
+const entradas = ref([
+  { name: 'Sunomono', unit: '(1 unidade)', description: ' Salada refrescante de pepino agridoce com gergelim torrado.', price: 'R$22,50', image: Sunomono },
+  { name: 'Gyoza', unit: '(4 unidades)', description: 'Delicados pasteizinhos recheados com carne suína, servidos com molho especial.', price: 'R$33,50', image: Gyoza },
+  { name: 'Tataki de Atum', unit: '(8 unidades)', description: 'Finas fatias de atum levemente selado, servidas com molho cítrico ponzu.', price: 'R$45,90', image: Tataki },
+])
+
+const bebidas = ref([
+  { name: 'Àgua sem gás', unit: '(Unidade 1)', description: 'Água Mineral Sem Gás - 510ml', price: 'R$8,00', image: aguasg },
+  { name: 'Àgua com Gás', unit: '(Unidade 1)', description: 'Água Mineral Com Gás - 510ml', price: 'R$8,00', image: aguacg },
+  { name: 'Refri', unit: '(Unidade 1)', description: 'Refrigerantes diversos. 220ml', price: 'R$6,50', image: refri },
+  { name: 'Sucos', description: 'Sucos naturais de frutos. 250ml', price: 'R$11,00', image: sucos },
+])
+
+const principais = ref([
+  { name: 'Destilados', unit: '(A partir de:)', description: ' Bebidas alcoólicas obtidas por destilação, geralmente mais fortes, feitas a partir de grãos, frutas ou cana-de-açúcar.', price: 'R$40,00', image: sushi1 },
+  { name: 'Vinhos', unit: '(A partir de:)', description: 'Bebidas fermentadas feitas a partir de uvas.', price: 'R$70,00', image: sushi2 },
+  { name: 'Drinks', unit: '(A partir de:)', description: 'Misturas de bebidas alcoólicas com outros ingredientes.', price: 'R$25,50', image: sushi3 },
+  { name: 'Cervejas', description: 'Bebidas fermentadas à base de cevada e lúpulo, com estilos variados.', unit: '(A partir de:)', price: 'R$12,50', image: sushi4 },
+  { name: 'Cervejas', description: 'Bebidas fermentadas à base de cevada e lúpulo, com estilos variados.', unit: '(A partir de:)', price: 'R$12,50', image: sushi5 }
+])
+
+const sobremesas = ref([
+  { name: 'Mochi', unit: '(2 unidades)', description: 'Doce japonês tradicional de massa de arroz, recheado.', price: 'R$32,50', image: mochi },
+  { name: 'Sorvete', unit: '(1 unidade)', description: 'Sobremesa cremosa com o sabor suave e característico', price: 'R$30,00', image: sorvete },
+  { name: 'Petit Gâteau', unit: '(1 unidade)', description: 'Pequeno bolo de chocolate com recheio cremoso, servido com sorvete de creme.', price: 'R$30,00', image: petitgateau },
+  { name: 'Dorayaki', description: 'Panqueca japonesa recheada com doce de feijão azuki, uma combinação de texturas e sabores.', unit: '(2 unidades)', price: 'R$25,00', image: dorayaki },
+  { name: 'Tempurá de Sorvete', description: 'Sorvete envolto em uma crosta crocante de tempurá, frito rapidamente e servido quente por fora e gelado por dentro.', unit: '(1 unidades)', price: 'R$30,00', image: tempura },
+  { name: 'Anmitsu', description: 'Doce tradicional japonês com cubos de gelatina de ágar, frutas, pasta de feijão doce e sorvete de chá verde, servido com calda de açúcar mascavo.', unit: '(1 unidades)', price: 'R$45,00', image: anmitsu },
+])
+</script>
+<template>
+  <div v-for="Bentradas in Bentradas" :key="Bentradas">
+    <Produtos :image="Entradas" :title="Entradas"/>
+  </div>
+  <div v-for="item in entradas" :key="item">
+    <Produtos :name="item.name" :description="item.description" :unit="item.unit" :price="item.price"
+      :imagem="item.image" />
+  </div>
+
+  <div v-for="item in principais" :key="item">
+    <Produtos :name="item.name" :description="item.description" :unit="item.unit" :price="item.price"
+      :imagem="item.image" />
+  </div>
+
+  <div v-for="item in bebidas" :key="item">
+    <Produtos :name="item.name" :description="item.description" :unit="item.unit" :price="item.price"
+      :imagem="item.image" />
+  </div>
+
+  <div v-for="item in bebidaAlcoolica" :key="item">
+    <Produtos :name="item.name" :description="item.description" :unit="item.unit" :price="item.price"
+      :imagem="item.image" />
+  </div>
+
+  <div v-for="item in sobremesas" :key="item">
+    <Produtos :name="item.name" :description="item.description" :unit="item.unit" :price="item.price"
+      :imagem="item.image" />
+  </div>
+</template>
