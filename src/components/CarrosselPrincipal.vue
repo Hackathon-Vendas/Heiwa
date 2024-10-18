e3bdc930ca21c3f0c3a8cfba41fbd3ff82d91fe2
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2024-10-18 09:28:21 -0300

    feat: fazendo carrossel #16

--- src/components/CarrosselPrincipal.vue
+++ src/components/CarrosselPrincipal.vue
@@ -1,158 +1,79 @@
-<script setup>
-let slideIndex = 1;
-import { onMounted } from 'vue';
-onMounted(() => {
-showSlides(slideIndex)
-});
+<script>
+const imagens = [
+  "img1.jpeg",
+  "img1.jpeg",
+  "img1.jpeg",
+]
+  // I  mport Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  // Import Swiper styles
+  import 'swiper/css';
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+  import 'swiper/css/navigation';
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+  // import required modules
+  import { Navigation } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Navigation],
+      };
+    },
+  };
 </script>
+
 <template>
-<!-- Slideshow container -->
-<div class="slideshow-container">
-
-    <!-- Full-width images with number and caption text -->
-    <div class="mySlides fade">
-      <div class="numbertext">1 / 3</div>
-      <img src="/public/img1.jpeg" style="width:100%">
-      <div class="text">Caption Text</div>
-    </div>
-  
-    <div class="mySlides fade">
-      <div class="numbertext">2 / 3</div>
-      <img src="/public/img1.jpeg" style="width:100%">
-      <div class="text">Caption Two</div>
-    </div>
-  
-    <div class="mySlides fade">
-      <div class="numbertext">3 / 3</div>
-      <img src="/public/img1.jpeg" style="width:100%">
-      <div class="text">Caption Three</div>
-    </div>
-  
-    <!-- Next and previous buttons -->
-    <a class="prev" @click="plusSlides(-1)">&#10094;</a>
-    <a class="next" @click="plusSlides(1)">&#10095;</a>
-  </div>
-  <br>
-  
-  <!-- The dots/circles -->
-  <div style="text-align:center">
-    <span class="dot" @click="currentSlide(1)"></span>
-    <span class="dot" @click="currentSlide(2)"></span>
-    <span class="dot" @click="currentSlide(3)"></span>
-  </div>
+  <swiper :rewind="true" :navigation="true" :modules="modules" class="mySwiper">
+    <swiper-slide v-for="imagem in imagens"><img :src="`public/${imagem}`" alt=""></swiper-slide>
+    </swiper>
 </template>
-<style scoped>
-* {box-sizing:border-box}
+<style>
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
+#app {
+  height: 100%;
+}
+html,
+body {
   position: relative;
-  margin: auto;
+  height: 100%;
 }
 
-/* Hide the images by default */
-.mySlides {
-  display: none;
+body {
+  background: #eee;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  color: #000;
+  margin: 0;
+  padding: 0;
 }
 
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
+.swiper {
   width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
   text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
