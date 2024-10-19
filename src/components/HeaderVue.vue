c25202acd2c9ff4a9a9d1dacf9e80513f9114d3c
gabrielimarta <gabrielirocha2730@gmail.com>
2024-10-18 23:12:54 -0300

    fix: Alteração de img para h1 na logo-secundaria #4

--- src/components/HeaderVue.vue
+++ src/components/HeaderVue.vue
@@ -1,47 +1,54 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <header>
-        <div class="container">
-            <img class="logo-principal" src="/public/logoH.png">
-            <img class="logo-secundaria" src="/public/logo-sushi.svg">
-        </div>
-        <div class="cart-icon">
-      <img src="/public/logo-carrinho.svg" alt="Carrinho de Compras" />
+  <header>
+    <div class="container">
+      <img class="logo-principal" src="/public/logoH.png" />
+      <h1 class="logo-secundaria">SUSHI & BAR</h1>
     </div>
-    </header>
+    <div class="cart-icon">
+      <img src="/public/logo-carrinho.svg" />
+    </div>
+  </header>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap'); /* Certifique-se de importar a fonte */
+
+
 header {
-    width: 100%;
-    padding: 7px;
-    margin: auto;
-    background-color: #1a1818;
-    position: static;
-    filter: drop-shadow(0px 15px 29px rgba(189, 188, 188, 0.438));
+  width: 100%;
+  padding: 7px;
+  margin: auto;
+  background-color: #1a1818;
+  position: static;
+  filter: drop-shadow(0px 15px 29px rgba(189, 188, 188, 0.438));
 }
 
 .container {
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-    margin: auto;
-    margin-top: 15px;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: auto;
+  margin-top: 15px;
+  justify-content: center;
+  align-items: center;
 }
 
 .logo-principal {
-    height: 54px;
-    width: 268px;
+  height: 54px;
+  width: 268px;
 }
 
 .logo-secundaria {
-    height: 17px;
-    width: 100px;
+  font-size: 14px;
+  font-family: 'Inter', sans-serif;
+  font-weight: 300; 
+  letter-spacing: 0.25em;
+  color: #fff;
 }
+
+
 
 .cart-icon {
   position: absolute;
@@ -50,9 +57,8 @@ header {
 }
 
 .cart-icon img {
-  width: 40px; 
+  width: 40px;
   height: auto;
   cursor: pointer;
 }
-
 </style>
