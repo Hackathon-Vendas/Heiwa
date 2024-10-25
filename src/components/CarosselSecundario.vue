5593c796c8c85e0833cc85ee3834a15930b1e0f4
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2024-10-25 09:30:51 -0300

    feat: fiz o carrossel #17

--- src/components/CarosselSecundario.vue
+++ src/components/CarosselSecundario.vue
@@ -1,39 +1,45 @@
-<script setup>
-
-</script>
-
 <template>
-    <div class="container">
-        <div class="carrossel">
-            <img src="/public/yakisoba.png" alt="">
-            <img src="/public/temakisalmao.png" alt="">
-            <img src="/public/temakiatum.png" alt="">
-            <img src="/public/urumaki.png" alt="">
-            <img src="/public/petitgateau.png" alt="">
-            <img src="/public/gyoza.png" alt="">
-            <img src="/public/anmitsu.png" alt="">
-
-        </div>
-    </div>
-</template>
-
-<style scoped>
-
-.container{
-    overflow: hidden;
-    height: 160px;
-    margin-left: 15vh;
-}
-.carrossel{
-    width: 86%;
-    display: flex;
-    margin-left: 57px ;
-    margin: auto;
-}
-img{
-    height: 160px;
-    width: 343px;
-    margin: 26px 0;
-}
-
-</style>
+    <swiper
+      :slidesPerView="3"
+      :spaceBetween="30"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide><img src="/public/anmitsu.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/gyoza.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/petitgateau.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/temakiatum.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/temakisalmao.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/urumaki.png" alt=""></swiper-slide>
+      <swiper-slide><img src="/public/yakisoba.png" alt=""></swiper-slide>
+    </swiper>
+  </template>
+  <script>
+    // Import Swiper Vue.js components
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+  
+    // Import Swiper styles
+    import 'swiper/css';
+  
+    import 'swiper/css/pagination';
+  
+  
+    // import required modules
+    import { Pagination } from 'swiper/modules';
+  
+    export default {
+      components: {
+        Swiper,
+        SwiperSlide,
+      },
+      setup() {
+        return {
+          modules: [Pagination],
+        };
+      },
+    };
+  </script>
+  
