5ecc7103c29452e1f5c97d268a9479cc93ac6ba4
gabrielimarta <gabrielirocha2730@gmail.com>
2024-11-01 13:46:19 -0300

    feat: iniciando desenvolvimento do modal de conta #40

---
+++ src/components/ContaModal.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const isModalOpen = ref(true);
+</script>
+
+<template>
+    <div v-if="isModalOpen" class="modal-over lay">
+        <div class="modal">
+            <button @click="closemodal" class="close-btn"> </button>
+            <h2>Conta</h2>
+            <div class="item-list">
+                <div v-for="iten in cardItens" :key="item.id" class="item">
+                    <span>{{ item.quality }}X</span>
+                    <span> {{ item.name }}</span>
+                    <span>R$ {{ item.price.toFixed(2) }}</span>
+
+                </div>
+            </div>
+            <div class="subtotal">
+                <span>SUBTOTAL:</span>
+                <span>R$ {{ item.subtotal.toFixed(2) }}</span>
+            </div>
+            <div class="total">
+                <span>TOTAL:</span>
+                <span>R$ {{ item.total.toFixed(2) }}</span>
+            </div>
+            <button class="finalizar"></button>
+        </div>
+    </div>
+</template>
