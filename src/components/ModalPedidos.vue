b6afc940f52afc17b27d6319a5d46e0eb702332f
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2024-11-02 12:53:17 -0300

    Feat: terminando modal pedidos #28

--- src/components/ModalPedidos.vue
+++ src/components/ModalPedidos.vue
@@ -6,7 +6,7 @@
         <div class="pedidos">
             <div class="informacoes">
                 <h1>PEDIDOS</h1>
-                <i>x</i>
+                <i><img src="/src/assets/excluir.png" alt=""></i>
             </div>
             <div class="itens">
                 <div class="item-1">
@@ -59,6 +59,7 @@
                 </div>
                 <div class="botoesPedido">
                     <button class="finalizarPedido">FINALIZAR PEDIDO</button>
+                    <h3>OU</h3>
                     <button class="pedirConta">PEDIR CONTA</button>
                 </div>
             </div>
@@ -66,6 +67,10 @@
     </div>
 </template>
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+*{
+    font-family: "Inter", sans-serif;;
+}
 .container{
     background-color: #2D2D2D;
     width: 65%;
@@ -89,13 +94,13 @@
     flex-grow: 1;
     margin: 43px 0;
     color: white;
-    font-size: 40px;
+    font-size: 2.5rem;
 }
 i{
     cursor: pointer;
     font-size: 60px;
     padding: 43px 0;
-    margin-right: 49px;
+    margin-right: 70px;
     color: white;
 }
 .itens{
@@ -103,7 +108,7 @@ i{
     flex-direction: column;
     width: 89%;
     margin: auto;
-    height: 20rem;
+    height: 64%;
     overflow-y: scroll;
 }
 .itens::-webkit-scrollbar{
@@ -116,19 +121,24 @@ i{
     align-items: center;
     background-color: #6B6B6B;
     height: 84px;
-     padding: 2rem 0;
+    padding: 2rem 0;
     border-radius: 20px;
     margin: 10px auto;
+    color: white;
+    font-weight: bold
 }
 .quantidade{
     font-size: 30px;
     padding-left: 30px;
+    border-right: px solid white; /* Adiciona uma linha vertical */
+    padding-right: 25.3px;
+    text-align: center;
 }
 .item{
     font-size: 20px;
-    padding-left: 30px;
     padding-right: 459px;
 }
+
 .valor{
     font-size: 25px;
     padding-right: 34px;
@@ -139,9 +149,44 @@ i{
    text-align: center;
    cursor: pointer;
 }
+.pedidoUsuario{
+    height: 212px;
+}
 .infoPedido{
     display: flex;
     justify-content: space-between;
-    text-align: center;
+    margin: 41px;
+    margin-bottom: 32px;
+}
+.infoPedido h3{
+    color: white;
+    font-size: 30px;
+}
+.botoesPedido{
+    display: flex;
+    justify-content: space-between;
+    margin: 32px 32px 49px 35px;
+}
+.botoesPedido button{
+    width: 391px;
+    height: 59px;
+    border-radius: 20px;
+    font-size: 30px;
+    color: white;
+    border: none;
+    cursor: pointer;
+    font-weight: bold
+}
+.botoesPedido h3{
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    color: #949494;
+}
+.finalizarPedido{
+    background-color: #B93333;
+}
+.pedirConta{
+    background-color: #FF8000;
 }
 </style>
