4e25a183f568dd5726fdbc206916bf9806e01fb3
vallentini <vallentinigabrielli.silva@gmail.com>
2024-12-02 09:06:17 -0300

    chore: adicionando function updateMenu() #11

--- src/components/MenuVue.vue
+++ src/components/MenuVue.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { RouterLink } from 'vue-router';
 import { useRoute } from 'vue-router';
 
@@ -14,39 +14,64 @@ const entradasR = ref(false);
 const principaisR = ref(false);
 const sobremesasR = ref(false);
 const sobremesas = ref(false);
+const route = useRoute();
+
+function updateMenu() {
+  const currentPath = route.path;
+
+  rodizio.value = false;
+  alacarte.value = false;
+  bebidas.value = false;
+  sobremesas.value = false;
+  nalcoolicas.value = false;
+  alcoolicas.value = false;
+  entradas.value = false;
+  principais.value = false;
+  entradasR.value = false;
+  principaisR.value = false;
+  sobremesasR.value = false;
+}
+
+watch(() => route.path, updateMenu, { immediate: true });
 
 function showAlacarte() {
-  if (alacarte.value) {
-    alacarte.value = false;
-  } else {
+    alacarte.value = !alacarte.value;
     alacarte.value = true;
     rodizio.value = false;
     bebidas.value = false;
     sobremesas.value = false;
   }
-}
+
 
 function showRodizio() {
-  if (rodizio.value) {
-    rodizio.value = false;
-  } else {
-    rodizio.value = true;
+    rodizio.value = !rodizio.value;
     alacarte.value = false;
     bebidas.value = false;
     sobremesas.value = false;
   }
-}
 
 function showBebidas() {
-  if (bebidas.value) {
-    bebidas.value = false;
-  } else {
-    bebidas.value = true;
+    bebidas.value = !bebidas.value;
     alacarte.value = false;
     rodizio.value = false;
     sobremesas.value = false;
-  }
+  
 }
+function scrollToSectionsobremesas() {
+    sobremesas.value = !sobremesas.value;
+    alacarte.value = false;
+    rodizio.value = false;
+    bebidas.value = false;
+    nalcoolicas.value = false;
+    alcoolicas.value = false;
+    entradas.value = false;
+    principais.value = false;
+    entradasR.value = false;
+    principaisR.value = false;
+    sobremesasR.value = false;
+
+  }
+
 function scrollToSection(section) {
   nalcoolicas.value = false;
   alcoolicas.value = false;
@@ -79,11 +104,10 @@ function scrollToSection(section) {
     case 'alcoolicas':
       alcoolicas.value = true;
       break;
-    case 'sobremesas':
-      sobremesas.value = true;
-      break;
   }
 }
+
+updateMenu();
 
 </script>
 <template>
@@ -123,13 +147,13 @@ function scrollToSection(section) {
           </li>
           <li>
             <RouterLink :to="{ path: '/produtos', hash: '#principaisR' }">
-              <button @click="scrollToSection('entradasR')" class="sub-menu"
+              <button @click="scrollToSection('principaisR')" class="sub-menu"
                 :class="{ 'selectedMenu': principaisR == true }">Pratos Principais</button>
             </RouterLink>
           </li>
           <li>
             <RouterLink :to="{ path: '/produtos', hash: '#sobremesasR' }">
-              <button @click="scrollToSection('principaisR')" class="sub-menu"
+              <button @click="scrollToSection('sobremesasR')" class="sub-menu"
                 :class="{ 'selectedMenu': sobremesasR == true }">Sobremesas</button>
             </RouterLink>
           </li>
@@ -143,7 +167,8 @@ function scrollToSection(section) {
         <ul>
           <li>
             <RouterLink :to="{ path: '/produtos', hash: '#bebidas' }">
-              <button @click="scrollToSection('nalcoolicas')" class="sub-menu" :class="{ 'selectedMenu': nalcoolicas == true }">Não
+              <button @click="scrollToSection('nalcoolicas')" class="sub-menu"
+                :class="{ 'selectedMenu': nalcoolicas == true }">Não
                 alcóolicas </button>
             </RouterLink>
           </li>
@@ -158,7 +183,7 @@ function scrollToSection(section) {
       </div>
       <li>
         <RouterLink :to="{ path: '/produtos', hash: '#sobremesa' }">
-          <button @click="scrollToSection('sobremesas')" :class="{ 'selectedMenu': sobremesas == true }"><img
+          <button @click="scrollToSectionsobremesas()" :class="{ 'selectedMenu': sobremesas == true }"><img
               src="/logo-sobremesa.svg">
             <p>Sobremesas</p>
           </button>
@@ -172,7 +197,8 @@ function scrollToSection(section) {
 aside {
   width: 200px;
   top: 85px;
-  height: calc(100% - 85px);  left: 0;
+  height: calc(100% - 85px);
+  left: 0;
   position: fixed;
   filter: drop-shadow(-1px 6.5px 25px rgba(0, 0, 0, 0.84));
   overflow-y: auto;
@@ -200,9 +226,9 @@ aside ul li {
   font-family: 'Inter', normal, sans-serif;
   font-weight: 900;
   font-size: 1rem;
-  padding: 3em;
+  padding: 20%;
   line-height: 17px;
-  text-align: center;
+  text-align: center !important;
   letter-spacing: 0.25em;
   color: #FFFFFF;
   background-color: rgba(69, 69, 69, 1);
