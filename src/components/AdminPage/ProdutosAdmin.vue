ca0feceb218a3bc7f6b44d523c83b6d91baa6678
Henrique0330 <henriquebelboni@gmail.com>
2024-12-04 12:46:21 -0300

    feat:  Alteração do componente 'ProdutosAdmin' #72

--- src/components/AdminPage/ProdutosAdmin.vue
+++ src/components/AdminPage/ProdutosAdmin.vue
@@ -1,7 +1,32 @@
 <script setup></script>
 
 <template>
-    <h1>Começo de um Projeto que estamos nos dedicando!!!!!</h1>
+    <div class="container">
+        <div class="product">
+            <div class="bottom-pronto"></div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container{
+    margin-top: 30px;
+    justify-content: center;
+    display: flex;
+}
+
+.product {
+    width: 400px;
+    height: 200px;
+    background-color: #575757;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column-reverse
+}
+.bottom-pronto{
+    width: 400px;
+    height: 55px;
+    background-color: #454545;
+    border-radius: 8px;
+}
+</style>
