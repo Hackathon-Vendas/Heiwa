e315d5b44bfcc2c59b8fb4151bf4860f4650e9a4
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2024-12-10 10:55:54 -0300

    feat-42

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,35 @@
 <script setup>
-import ContaModal from '@/components/ContaModal.vue';
 import { ref } from 'vue';
-import MenuVue from '@/components/MenuVue.vue'
-import HeaderVue from '@/components/HeaderVue.vue'
-import NumeroMesa from '@/components/NumeroMesa.vue';
-import CarrosselPrincipal from '@/components/CarrosselPrincipal.vue';
-import ModalPedidos from '@/components/ModalPedidos.vue';
-import InicioVue from '@/components/InicioVue.vue';
-import CarrosselSecundario from '@/components/CarrosselSecundario.vue';
-import {arM} from '@/stores/armazenamento';
+import { arM } from '@/stores/armazenamento';
 
-const ModalA = ref(0);
-const guarda = arM()
+import InicioVue from '@/components/InicioVue.vue';
+import NumeroMesa from '@/components/NumeroMesa.vue';
+import MenuVue from '@/components/MenuVue.vue';
+import HeaderVue from '@/components/HeaderVue.vue';
+import RodizioVue from '@/components/RodizioVue.vue'; // Certifique-se de que este componente está implementado
+import ContaModal from '@/components/ContaModal.vue';
+import ModalPedidos from '@/components/ModalPedidos.vue';
+import CarrosselPrincipal from '@/components/CarrosselPrincipal.vue';
+import CarrosselSecundario from '@/components/CarrosselSecundario.vue';
+
+let ModalA = ref(0); // Controla qual modal está sendo exibido
+const guarda = arM();
 </script>
 
 <template>
   <MenuVue />
   <HeaderVue />
-  <InicioVue v-if="!guarda.getVisto().value"; @modal="ModalA++; guarda.setVisto(true)"/>
-  <NumeroMesa v-if="ModalA == 1 && guarda.getNmesa().value==null" />
-  <RodizioVue v-if="ModalA == 2 && guarda.getRodizio().value==null" @voltarParaMesa="ModalA--; guarda.setRodizio(true)" @FinalModal="ModalA++" />
+
+  <InicioVue v-if="ModalA === 0" @modal="ModalA++" />
+  <NumeroMesa v-if="ModalA === 1 && guarda.getNmesa().value === null" @avancar="ModalA++" />
+  <RodizioVue 
+    v-if="ModalA === 2 && guarda.getRodizio().value === null" 
+    @voltarParaMesa="ModalA--" 
+    @FinalModal="ModalA++" 
+  />
+
   <main>
-    <ContaModal/>
+    <ContaModal />
     <ModalPedidos />
     <CarrosselPrincipal />
     <CarrosselSecundario />
@@ -33,6 +41,4 @@ main {
   margin-left: 15%;
   margin-top: 8%;
 }
-
-
 </style>
